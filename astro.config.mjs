bf30e60020eb693ca79bb3fdeb99883c6ec98c72
Denis Stuk <denstuk@outlook.com>
2024-09-17 16:54:42 +0300

    feat: try to remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://denstuk.github.io",
-  base: "/denstuk.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
   output: "static"
 });
